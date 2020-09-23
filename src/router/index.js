3cfc665dc047840ec8b890bcea2d68fbf06e8b1b
zzwh5 <195917428@qq.com>
2020-09-23 14:02:52 +0800

    app

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,41 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import House from "../views/House.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: "/house",
+    name: "House",
+    component: House
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/addEstateOrFloor",
+    name: "AddEstateOrFloor",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/add/addEstateOrFloor.vue")
+  },
+  {
+    path: "/floor",
+    name: "Floor",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/house/floor.vue")
+  },
+  {
+    path: "/estateInfo",
+    name: "EstateInfo",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/house/estateInfo.vue")
   }
 ];
 
